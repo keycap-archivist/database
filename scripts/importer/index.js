a4f378639a8fec57a129b9812f883c78afc2234e
Vincent LE GOFF <vince.legoff@gmail.com>
2020-03-24 11:48:06 +0100

    Alphabetical order and parrallelism

--- scripts/importer/index.js
+++ scripts/importer/index.js
@@ -19,12 +19,12 @@ const path = require("path");
 const DEST = path.resolve(path.join(__dirname, "..", "..", "db"));
 const DESTINATION_JSON = path.join(DEST, "catalog.json");
 const DESTINATION_CSV = path.join(DEST, "catalog.csv");
-const catalog = [];
+let catalog = [];
 
 function flatten() {
   const arr = [];
   const outArtist = {};
-  for (const artist in catalog) {
+  for (const artist in Object.keys(catalog).sort()) {
     const artistArr = [];
     for (const sculpt in catalog[artist].sculpts) {
       catalog[artist].sculpts[sculpt].colorways.forEach(s => {
@@ -44,16 +44,27 @@ function flatten() {
   return { full: arr, artist: outArtist };
 }
 
+async function moduleScrap(moduleName) {
+  const m = require(moduleName);
+  const _catalog = await m.scrap();
+  catalog.push(_catalog);
+  fs.writeFileSync(
+    path.join(DEST, `${_catalog.name.toLowerCase().replace(/ /g, "-")}.json`),
+    JSON.stringify(_catalog)
+  );
+}
+
 async function main() {
+  const p = [];
   for (const s of scraps) {
-    const m = require(s);
-    const _catalog = await m.scrap();
-    catalog.push(_catalog);
-    fs.writeFileSync(
-      path.join(DEST, `${_catalog.name.toLowerCase().replace(/ /g, "-")}.json`),
-      JSON.stringify(_catalog)
-    );
+    p.push(moduleScrap(s));
   }
+  await Promise.all(p);
+  catalog = catalog.sort(function(a, b) {
+    const textA = a.name.toUpperCase();
+    const textB = b.name.toUpperCase();
+    return textA < textB ? -1 : textA > textB ? 1 : 0;
+  });
   const flattennedCatalog = flatten();
   fs.writeFileSync(DESTINATION_JSON, JSON.stringify(catalog));
   fs.writeFileSync(
