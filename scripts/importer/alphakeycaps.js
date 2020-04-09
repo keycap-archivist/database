5fad039f151481fd84a3538c7c3227fdab5c6ab3
Vincent LE GOFF <vince.legoff@gmail.com>
2020-04-09 19:07:18 +0200

    Updating alpha keycaps importer

--- scripts/importer/alphakeycaps.js
+++ scripts/importer/alphakeycaps.js
@@ -9,33 +9,30 @@ async function scrap() {
     name: "Alpha Keycaps",
     instagram: "https://www.instagram.com/alphakeycaps/",
     website: "https://alphakeycaps.com",
-    sculpts: [{ name: "keypora", id: genId("keypora"), colorways: [] }]
+    sculpts: [{ name: "keypora", id: genId("keypora"), colorways: [] }],
   };
   const url = "https://alphakeycaps.com/keypora";
-  const html = await axios.get(url).then(res => {
+  const html = await axios.get(url).then((res) => {
     return res.data;
   });
   const root = htmlparser.parse(html);
-  const colorways = root.querySelectorAll(".intrinsic").map(e => {
-    const name = e
-      .querySelector(".image-caption")
-      .querySelector("p")
-      .querySelector("strong").childNodes[0].rawText;
+  const colorways = root.querySelectorAll(".intrinsic").map((e) => {
+    const name = e.querySelector(".image-caption").querySelector("p").querySelector("strong").childNodes[0].rawText;
     const rawImg = e.querySelector(".thumb-image").rawAttrs;
     const m = /data-src="(.*?)"/.exec(rawImg);
     const imgsrc = m[1];
-    return { name: name, img: imgsrc, id: genId(imgsrc) };
+    return { name: name.replace(/keypora/gi, "").trim(), img: imgsrc, id: genId(imgsrc) };
   });
   catalog.sculpts[0].colorways = colorways;
   return catalog;
 }
 
 if (require.main === module) {
-  scrap().then(catalog => {
+  scrap().then((catalog) => {
     fs.writeFileSync("alphakeycaps.json", JSON.stringify(catalog));
   });
 }
 
 module.exports = {
-  scrap
+  scrap,
 };
