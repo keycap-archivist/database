4d84ce23ef830de0524f106eb1ddbd47dabaf37d
Vincent LE GOFF <vince.legoff@gmail.com>
2020-06-20 17:20:08 +0200

    feat: add lividity

---
+++ scripts/importer/lividity.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const htmlparser = require('node-html-parser');
+const { downloadFile, genId, gDriveParse, gDocUrl } = require('./utils');
+
+const GDOC_ID = '18mc8abYoFIYDEqpvJzG5qYLXoQBTBUu9DUp7JSmIFxM';
+
+async function scrap() {
+  const index = await downloadFile(GDOC_ID);
+  const rootNode = htmlparser.parse(index);
+  const tabs = rootNode.querySelectorAll('table');
+  const catalog = {
+    src: gDocUrl(GDOC_ID),
+    id: genId('Lividity'),
+    name: 'Lividity',
+    instagram: 'https://www.instagram.com/lividitycaps/',
+    website: '',
+    sculpts: [],
+  };
+  return gDriveParse(catalog, tabs);
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync('lividity.json', JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
