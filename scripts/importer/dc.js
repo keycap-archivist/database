2398d5c08f6257c7203b9bc29b468c5996122e65
Vincent LE GOFF <vince.legoff@gmail.com>
2020-07-05 23:07:06 +0200

    feat: add dc caps

---
+++ scripts/importer/dc.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const htmlparser = require('node-html-parser');
+const { downloadFile, genId, gDriveParse, gDocUrl } = require('./utils');
+
+const GDOC_ID = '1ENp3M-HymI9LsJRloDGuo3o9s-FXHzFj6hWazGfXSEQ';
+
+async function scrap() {
+  const index = await downloadFile(GDOC_ID);
+  const rootNode = htmlparser.parse(index);
+  const tabs = rootNode.querySelectorAll('table');
+  tabs.pop(); // credit
+  const catalog = {
+    src: gDocUrl(GDOC_ID),
+    id: genId('DCcaps'),
+    name: 'DCcaps',
+    instagram: 'https://www.instagram.com/dccaps/',
+    website: '',
+    sculpts: [],
+  };
+  return gDriveParse(catalog, tabs);
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync('dc.json', JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
