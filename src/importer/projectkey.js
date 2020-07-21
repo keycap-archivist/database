9922ca9649446a3c033c9c3b07faf0c4bff82717
Vincent LE GOFF <vince.legoff@gmail.com>
2020-07-21 09:01:35 +0200

    feat: add projectkey

---
+++ src/importer/projectkey.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const htmlparser = require('node-html-parser');
+const { downloadFile, genId, gDriveParse, gDocUrl } = require('../utils');
+
+const GDOC_ID = '1UGwGloN5Cf6w_goJB6FX3FcDiB8EsCjHLLB4r17T3OE';
+
+async function scrap() {
+  const index = await downloadFile(GDOC_ID);
+  const rootNode = htmlparser.parse(index);
+  const tabs = rootNode.querySelectorAll('table');
+  const catalog = {
+    src: gDocUrl(GDOC_ID),
+    id: genId('ProjectKey'),
+    name: 'ProjectKey',
+    instagram: 'https://www.instagram.com/projectkey.hk',
+    website: '',
+    sculpts: [],
+  };
+  return gDriveParse(catalog, tabs);
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync('projectkey.json', JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
