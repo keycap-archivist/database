04cd9ac6f18b7b81f9e2c549802010419e6183cc
Vincent LE GOFF <vince.legoff@gmail.com>
2020-10-22 22:56:22 +0200

    feat: fix kbk vandal name

--- src/importer/kbk.js
+++ src/importer/kbk.js
@@ -6,7 +6,7 @@ const GDOC_ID = '1656SEjL_uolfVYeUgiAjbelyM_HhxAg77oTCPWiviD8';
 
 async function scrap() {
   const index = await downloadFile(GDOC_ID);
-  const rootNode = htmlparser.parse(index);
+  const rootNode = htmlparser.parse(index.replace('', ''));
   const tabs = rootNode.querySelectorAll('table');
   tabs.pop(); // credit
   const catalog = {
@@ -23,6 +23,9 @@ async function scrap() {
 
 if (require.main === module) {
   scrap().then((catalog) => {
+    // eslint-disable-next-line no-param-reassign
+    catalog.sculpts.find((x) => x.id === '76a63754').colorways.find((x) => x.id === '46f4d447').name =
+      'Proto Purle - "Kevin"';
     fs.writeFileSync('kbk.json', JSON.stringify(catalog));
   });
 }
