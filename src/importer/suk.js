23dedbe46cfdddd5e71a0d1bfe5b7fbd2fd8f23a
Vincent LE GOFF <vince.legoff@gmail.com>
2020-10-24 11:54:03 +0200

    feat: add SUK

---
+++ src/importer/suk.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const htmlparser = require('node-html-parser');
+const { downloadFile, genId, gDriveParse, gDocUrl, isSelfOrdered } = require('../utils');
+
+const GDOC_ID = '17XHW8yaMXVS5i82lOrjXIF7Q68NwCg6w9B6BiEn7A1k';
+
+async function scrap() {
+  const index = await downloadFile(GDOC_ID);
+  const rootNode = htmlparser.parse(index);
+  const tabs = rootNode.querySelectorAll('table');
+  tabs.pop(); // credit
+  const catalog = {
+    src: gDocUrl(GDOC_ID),
+    id: genId('Suited Up Keycaps'),
+    name: 'Suited Up Keycaps',
+    instagram: 'https://www.instagram.com/suitedupsuitedup/',
+    website: 'https://suitedupkeycaps.com/',
+    selfOrder: isSelfOrdered(index),
+    sculpts: [],
+  };
+  return gDriveParse(catalog, tabs);
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync('suk.json', JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
