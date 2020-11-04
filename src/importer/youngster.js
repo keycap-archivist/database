59eb989cd304e535207799c030ad6e8609da76cb
Vincent LE GOFF <vince.legoff@gmail.com>
2020-11-04 14:22:03 +0100

    feat: add youngsterharris

---
+++ src/importer/youngster.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const htmlparser = require('node-html-parser');
+const { downloadFile, genId, gDriveParse, gDocUrl, isSelfOrdered } = require('../utils');
+
+const GDOC_ID = '1OB-qI-3izrbmWZG08Qi-ihNaCRRB_qmvivs0KCEWpAk';
+
+async function scrap() {
+  const index = await downloadFile(GDOC_ID);
+  const rootNode = htmlparser.parse(index);
+  const tabs = rootNode.querySelectorAll('table');
+  const catalog = {
+    src: gDocUrl(GDOC_ID),
+    id: genId('YoungsterHarris'),
+    name: 'YoungsterHarris',
+    instagram: 'https://www.instagram.com/youngsterharris/',
+    website: '',
+    selfOrder: isSelfOrdered(index),
+    sculpts: [],
+  };
+  return gDriveParse(catalog, tabs);
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync('youngster.json', JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
