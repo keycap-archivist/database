797accfce8e9f1f0fc108f7177a1e9e774c009e1
Vincent LE GOFF <vince.legoff@gmail.com>
2020-11-16 19:45:04 +0100

    fix: name

--- src/importer/gooey.js
+++ src/importer/gooey.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { scrapFrom } = require('../scraper/gdoc');
 
 const scrap = scrapFrom('15tTzH6j6qne6WHnADVr_kHI_n2WJUD7Zk26Qdiu_SLA', {
-  name: 'Gooey',
+  name: 'gooey keys',
   instagram: 'https://www.instagram.com/gooeykeys/',
   discord: 'https://discord.com/invite/Wuz3dS2hkX',
   website: 'https://www.gooeykeys.com/',
