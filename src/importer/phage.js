172248069e94252d38f1d46626d90edce0badadf
Vincent LE GOFF <vince.legoff@gmail.com>
2020-11-25 21:46:40 +0100

    feat: add phage caps

---
+++ src/importer/phage.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { scrapFrom } = require('../scraper/gdoc');
+
+const scrap = scrapFrom(
+  '1m7S6LaNaAg7vfZP9Bt23EYX7v3boC3TpTaqF0gBsEaM',
+  {
+    name: 'Phage Caps Design',
+    instagram: 'https://www.instagram.com/phage.caps/',
+  },
+  ['pop'],
+);
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync(`${path.basename(__filename, path.extname(__filename))}.json`, JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
