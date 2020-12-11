554ed15947d79d583c44b00a458c581280c06d2d
Guillaume Gérard <1322081+GreatWizard@users.noreply.github.com>
2020-12-11 12:10:45 +0100

    Add discord link for YoungsterHarris

--- src/importer/youngster.js
+++ src/importer/youngster.js
@@ -5,6 +5,7 @@ const { scrapFrom } = require('../scraper/gdoc');
 const scrap = scrapFrom('1OB-qI-3izrbmWZG08Qi-ihNaCRRB_qmvivs0KCEWpAk', {
   name: 'YoungsterHarris',
   instagram: 'https://www.instagram.com/youngsterharris/',
+  discord: 'https://discord.com/invite/Dv88j529',
 });
 
 if (require.main === module) {
