e00d501985347bbe2b308e15cb33a05fd1ac5c42
mariotheguy <56885146+mariotheguy@users.noreply.github.com>
2021-03-05 03:21:26 -0500

    Added hellbent caps catalog (#21)

---
+++ src/importer/hellbentcaps.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const { scrapFrom } = require('../scraper/gdoc');
+
+const scrap = scrapFrom(
+  '15NNk_ttTXCBCHTzyEE3_EigOHPYnc1M5fVGRZJIt0AA',
+  {
+    name: 'Hellbent Caps',
+    instagram: 'https://www.instagram.com/hellbentcaps/',
+  },
+  ['pop'],
+);
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync(`${path.basename(__filename, path.extname(__filename))}.json`, JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
