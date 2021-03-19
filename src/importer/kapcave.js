79725cf5bb6c0a428ba4d85e92e959d723540817
nachie <me@nachie.com>
2021-03-19 13:36:04 -0700

    Adding a importer for KapCave which uses the API to fetch catalog (#29)
    * Adding a importer for KapCave which uses the API to fetch catalog
    
    * Add ID generation for the main catalog dict
    
    * fixup
    
    Co-authored-by: Nachiket Kulkarni <nachiket_kulkarni@apple.com>

---
+++ src/importer/kapcave.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const { genId, sortBy } = require('../utils');
+
+const BASE_URL = 'https://kapcave.nachie.com/api/v1/catalog/list';
+
+async function scrap() {
+  try {
+    const catalog = await axios.get(`${BASE_URL}`).then((res) => res.data);
+    catalog.id = genId(`KapCave`);
+    for (const c of catalog.sculpts) {
+     c.id = genId(`KapCave-${c.name}`)
+    }
+    catalog.sculpts = sortBy(catalog.sculpts, 'name');
+    return catalog;
+  } catch (e) {
+    return {
+      name: 'KapCave',
+      hasError: true,
+      error: e,
+    };
+  }
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync(`${path.basename(__filename, path.extname(__filename))}.json`, JSON.stringify(catalog));
+  });
+}
+
+module.exports = {
+  scrap,
+};
