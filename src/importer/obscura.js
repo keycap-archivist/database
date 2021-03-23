4f35412bab9a18cba0874d4c7024a92bbd4e4890
mariotheguy <mariodandrea14@yahoo.com>
2021-03-23 17:14:54 -0400

    Removed pop from obscura

--- src/importer/obscura.js
+++ src/importer/obscura.js
@@ -1,14 +1,10 @@
 const { scrapFrom } = require('../scraper/gdoc');
 const { launcher } = require('../utils');
 
-const scrap = scrapFrom(
-  '1nhTGlHevXa6adT_nUrr8aSwt0MevlqlRA3K4_AAr7nE',
-  {
-    name: 'Obscura',
-    instagram: 'https://www.instagram.com/obscuracaps/',
-  },
-  ['pop'],
-);
+const scrap = scrapFrom('1nhTGlHevXa6adT_nUrr8aSwt0MevlqlRA3K4_AAr7nE', {
+  name: 'Obscura',
+  instagram: 'https://www.instagram.com/obscuracaps/',
+});
 
 launcher(scrap);
 
