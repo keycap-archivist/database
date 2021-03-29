c34e7b50dfaeea9c75d7ab79f389506f33fcfb5c
mariotheguy <56885146+mariotheguy@users.noreply.github.com>
2021-03-29 13:05:29 -0400

    added coconut keycaps catalog (#35)
    * added coconut keycaps catalog
    
    * Added pop arg

---
+++ src/importer/coconutkeycaps.js
@@ -0,0 +1,16 @@
+const { scrapFrom } = require('../scraper/gdoc');
+const { launcher } = require('../utils');
+
+const scrap = scrapFrom('1lx648GXchw4ZpctFtirPx_teQcpcgVWuy83W32JY4KU', 
+  {
+    name: 'Coconut Keycaps',
+    instagram: 'https://www.instagram.com/coconut.keycaps/',
+  },
+  ['pop'],
+);
+
+launcher(scrap);
+
+module.exports = {
+  scrap,
+};
