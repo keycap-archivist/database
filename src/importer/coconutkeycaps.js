866d109011673c3017e4a38603679fd1eafa3b0d
mariotheguy <mariodandrea14@yahoo.com>
2021-03-29 13:00:13 -0400

    Added pop arg

--- src/importer/coconutkeycaps.js
+++ src/importer/coconutkeycaps.js
@@ -1,10 +1,13 @@
 const { scrapFrom } = require('../scraper/gdoc');
 const { launcher } = require('../utils');
 
-const scrap = scrapFrom('1lx648GXchw4ZpctFtirPx_teQcpcgVWuy83W32JY4KU', {
-  name: 'Coconut Keycaps',
-  instagram: 'https://www.instagram.com/coconut.keycaps/',
-});
+const scrap = scrapFrom('1lx648GXchw4ZpctFtirPx_teQcpcgVWuy83W32JY4KU', 
+  {
+    name: 'Coconut Keycaps',
+    instagram: 'https://www.instagram.com/coconut.keycaps/',
+  },
+  ['pop'],
+);
 
 launcher(scrap);
 
