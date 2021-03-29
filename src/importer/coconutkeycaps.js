51c7881ee92f8402eb03c95d00e9eaa021e3e86f
mariotheguy <mariodandrea14@yahoo.com>
2021-03-29 12:46:40 -0400

    added coconut keycaps catalog

---
+++ src/importer/coconutkeycaps.js
@@ -0,0 +1,13 @@
+const { scrapFrom } = require('../scraper/gdoc');
+const { launcher } = require('../utils');
+
+const scrap = scrapFrom('1lx648GXchw4ZpctFtirPx_teQcpcgVWuy83W32JY4KU', {
+  name: 'Coconut Keycaps',
+  instagram: 'https://www.instagram.com/coconut.keycaps/',
+});
+
+launcher(scrap);
+
+module.exports = {
+  scrap,
+};
