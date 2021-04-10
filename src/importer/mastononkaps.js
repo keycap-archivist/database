6309cddbce1dfbb2aa086f380da9a9ed96e8ba5e
mariotheguy <56885146+mariotheguy@users.noreply.github.com>
2021-04-10 15:00:12 -0400

    Added mastononkaps catalog (#37)

---
+++ src/importer/mastononkaps.js
@@ -0,0 +1,16 @@
+const { scrapFrom } = require('../scraper/gdoc');
+const { launcher } = require('../utils');
+
+const scrap = scrapFrom('1XwH1785exI1NCfr3M-ZtkcTxDe4fICVP0Tmbvg00HXQ', 
+  {
+    name: 'Mastonon.kaps',
+    instagram: 'https://www.instagram.com/mastonon.kaps/',
+    discord:'https://discord.gg/ySC64us',
+  },
+);
+
+launcher(scrap);
+
+module.exports = {
+  scrap,
+};
