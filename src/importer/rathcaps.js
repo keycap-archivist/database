cfff1447e32d617ed1aa7dcf701ec47cb1600ab5
mariotheguy <56885146+mariotheguy@users.noreply.github.com>
2021-04-15 14:27:24 -0400

    Updated rathcaps catalog (#38)

--- src/importer/rathcaps.js
+++ src/importer/rathcaps.js
@@ -1,7 +1,7 @@
 const { scrapFrom } = require('../scraper/gdoc');
 const { launcher } = require('../utils');
 
-const scrap = scrapFrom('17xkdiEkCjV-4bRoLg5FgXGxlPDX1NPLCZjWIasnFaeY', {
+const scrap = scrapFrom('1mX-wUILSjmOGULIju5SKD3zKn7VE9hLl2BHgZZPTnG0', {
   name: 'Rathcaps',
   instagram: 'https://www.instagram.com/rathcaps/',
   discord: 'https://discord.com/invite/fnVPZpX',
