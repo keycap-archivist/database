5f61f6de5936e5d17712bf566ad5fd75f8b35aac
Anh Thang <buianhthang89@gmail.com>
2021-04-21 21:47:04 +0700

    feat: update trmk discord, website

--- src/importer/trmk.js
+++ src/importer/trmk.js
@@ -4,7 +4,8 @@ const { launcher } = require('../utils');
 const scrap = scrapFrom('19r0FNbI7cLrjogfDviC7WT2HkuRax9RmPZwn3B_DNOY', {
   name: 'trmk',
   instagram: 'https://www.instagram.com/trmkcaps/',
-  website: 'https://geekhack.org/index.php?topic=101348.msg2780216#msg2780216',
+  website: 'https://trmkcaps.com/',
+  discord: 'https://discord.com/invite/YrV6axHHze',
 });
 
 launcher(scrap);
