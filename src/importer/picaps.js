04489ba908c3ebe8efe8e54ca5f3416722a4a5cb
Anh Thang <buianhthang89@gmail.com>
2021-05-02 18:47:57 +0700

    feat: add Picaps' discord (#44)

--- src/importer/picaps.js
+++ src/importer/picaps.js
@@ -4,6 +4,7 @@ const { launcher } = require('../utils');
 const scrap = scrapFrom('1SyKkp4PhrCCealubpwHz5GUWfZejQGspXmgWW2T0qzw', {
   name: 'Picaps',
   instagram: 'https://www.instagram.com/pizzza013/',
+  discord: 'https://discord.gg/tfmUzNuySv',
 });
 
 launcher(scrap);
