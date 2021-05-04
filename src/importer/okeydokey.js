82132925be2f815993d09f2f9080e9056ec180b9
Anh Thang <buianhthang89@gmail.com>
2021-05-04 19:22:15 +0700

    Update okeydokey studio name (#47)
    * Update name as requested from the maker
    
    * fix: keep old maker id

--- src/importer/okeydokey.js
+++ src/importer/okeydokey.js
@@ -1,8 +1,9 @@
 const { scrapFrom } = require('../scraper/gdoc');
-const { launcher } = require('../utils');
+const { genId, launcher } = require('../utils');
 
 const scrap = scrapFrom('1_khNYhY3YlyeRK9Tok3qDQ-FJU-lBittK0fsNiLbWhI', {
-  name: 'okeydokey',
+  id: genId("okeydokey"),
+  name: 'okeydokey studio',
   website: 'https://okeydokey.studio/',
   instagram: 'https://www.instagram.com/okeydokey.studio/',
   discord: 'https://discord.com/invite/8EDEy94zwJ',
