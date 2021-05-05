8bc998b7a208d709ad507ef2095e5839f8082a25
Anh Thang Bui <buianhthang89@gmail.com>
2021-05-05 14:21:49 +0700

    fix: dont care about the id

--- src/importer/okeydokey.js
+++ src/importer/okeydokey.js
@@ -1,8 +1,7 @@
 const { scrapFrom } = require('../scraper/gdoc');
-const { genId, launcher } = require('../utils');
+const { launcher } = require('../utils');
 
 const scrap = scrapFrom('1_khNYhY3YlyeRK9Tok3qDQ-FJU-lBittK0fsNiLbWhI', {
-  id: genId("okeydokey"),
   name: 'okeydokey studio',
   website: 'https://okeydokey.studio/',
   instagram: 'https://www.instagram.com/okeydokey.studio/',
