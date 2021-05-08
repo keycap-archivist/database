340a66b5e209956fb8c5a568440c8bd49ad92f36
mariotheguy <mariodandrea14@yahoo.com>
2021-05-08 00:59:52 -0400

    Added ochre and moss catalog

---
+++ src/importer/ochre+moss.js
@@ -0,0 +1,17 @@
+const { scrapFrom } = require('../scraper/gdoc');
+const { launcher } = require('../utils');
+
+const scrap = scrapFrom(
+  '1r3HhhDyLTnLJrDb5kpCazx_-j7SwOk9GHl9G-zek-N0',
+  {
+    name: 'Ochre + Moss',
+    instagram: 'https://www.instagram.com/ochre.moss/',
+    discord: 'https://discord.gg/EAHpSPfTE6/', 
+  }
+);
+
+launcher(scrap);
+
+module.exports = {
+  scrap,
+};
