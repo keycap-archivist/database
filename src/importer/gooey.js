ddeb61eae1c8326fceab3a82647754f3dd217bd6
ryan | gooey keys <74985611+gooeykeys@users.noreply.github.com>
2021-06-13 12:25:59 -0500

    Custom Gooey Keys importer
    Heyo!
    
    I've exhausted my patience with Google Docs, so I've adapted the work by Zekth, GreatWizard & nachie into a somewhat more generalized -- i.e. all of the variables have been pulled out of the logic -- custom importer for complete (except for the IDs) JSON catalogs.
    
    Please let me know if there are any issues here, either in the code itself or the general concept. I'm happy to make any changes or discuss as needed.
    
    One thing that could be an issue (specifically for my catalog) is that I've capitalized my artisan name. It is currently all lowercase on the site, but I'd like it to be in capital case.
    
    As always, thanks for y'all's work on the archive and for your time!

--- src/importer/gooey.js
+++ src/importer/gooey.js
@@ -1,14 +1,54 @@
-const { scrapFrom } = require('../scraper/gdoc');
-const { launcher } = require('../utils');
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+const { genId, sortBy } = require('../utils');
 
-const scrap = scrapFrom('15tTzH6j6qne6WHnADVr_kHI_n2WJUD7Zk26Qdiu_SLA', {
-  name: 'gooey keys',
-  instagram: 'https://www.instagram.com/gooeykeys/',
-  discord: 'https://discord.com/invite/Wuz3dS2hkX',
-  website: 'https://www.gooeykeys.com/',
-});
+const CATALOG_JSON_URL = 'https://gooey.link/keycap-archivist.json';
+const ARTISAN_NAME = 'Gooey Keys';
+const ARTISAN_NAME_KEBAB_CASE = 'gooey-keys';
+const USE_PROVIDED_SORTING = true;
 
-launcher(scrap);
+async function scrap() {
+  try {
+    const catalog = await axios.get(CATALOG_JSON_URL).then((res) => res.data);
+
+    // Generate a unique ID for the catalog
+    catalog.id = genId(ARTISAN_NAME);
+
+    // Generate unique IDs for each sculpt
+    for (const sculpt of catalog.sculpts) {
+      sculpt.id = genId(`${ARTISAN_NAME_KEBAB_CASE}-${sculpt.name}`);
+      
+      if(!USE_PROVIDED_SORTING) {
+        sculpt.colorways = sortBy(sculpt.colorways, 'name');
+      }
+
+      // Generate unique IDs for each colorway
+      for (const colorway of sculpt.colorways) {
+        colorway.id = genId(colorway.img);
+      }
+    }
+    
+    if(!USE_PROVIDED_SORTING) {
+      catalog.sculpts = sortBy(catalog.sculpts, 'name');
+    }
+
+    return catalog;
+
+  } catch (e) {
+    return {
+      name: ARTISAN_NAME,
+      hasError: true,
+      error: e,
+    };
+  }
+}
+
+if (require.main === module) {
+  scrap().then((catalog) => {
+    fs.writeFileSync(`${path.basename(__filename, path.extname(__filename))}.json`, JSON.stringify(catalog));
+  });
+}
 
 module.exports = {
   scrap,
