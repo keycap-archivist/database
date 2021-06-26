14e7bca747edbef3b0eea0a9a39cd7c14f154ce6
Vincent LE GOFF <vince.legoff@gmail.com>
2021-06-26 12:44:34 +0200

    feat: update goth invite

--- src/importer/goth.js
+++ src/importer/goth.js
@@ -4,7 +4,7 @@ const { launcher } = require('../utils');
 const scrap = scrapFrom('17YdYcvKifysUDk6mt750Jgq7Zke2kSi66kfqc9BE6RQ', {
   name: 'Gothcaps',
   instagram: 'https://www.instagram.com/gothcaps/',
-  discord: 'https://discord.com/invite/ncm4K6D',
+  discord: 'https://discord.gg/zCx5PXkSkD',
 });
 
 launcher(scrap);
