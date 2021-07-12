698097ed9bece29b4c23cf906eaadbb7cb612b2f
Anh Thang <buianhthang89@gmail.com>
2021-07-12 21:51:55 +0700

    fix: Dreadkeys discord invite

--- src/importer/dreadkeys.js
+++ src/importer/dreadkeys.js
@@ -5,7 +5,7 @@ const scrap = scrapFrom('1tzm7_NLV5XdM2tuM2naiZhDjMMygstN2XneBU_GWbeY', {
   name: 'Dreadkeys',
   website: 'https://www.dreadkeys.com/',
   instagram: 'https://www.instagram.com/dreadkeys/',
-  discord: 'https://discord.gg/KDFs857bnA',
+  discord: 'https://discord.gg/cDdFgyYjWx',
 });
 
 launcher(scrap);
