d83be3e119ce103803350eec55962f05aedaaee6
Anh Thang <buianhthang89@gmail.com>
2021-07-12 21:50:40 +0700

    feat: add Dreadkeys website

--- src/importer/dreadkeys.js
+++ src/importer/dreadkeys.js
@@ -3,6 +3,7 @@ const { launcher } = require('../utils');
 
 const scrap = scrapFrom('1tzm7_NLV5XdM2tuM2naiZhDjMMygstN2XneBU_GWbeY', {
   name: 'Dreadkeys',
+  website: 'https://www.dreadkeys.com/',
   instagram: 'https://www.instagram.com/dreadkeys/',
   discord: 'https://discord.gg/KDFs857bnA',
 });
