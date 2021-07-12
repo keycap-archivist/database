ee4686f839e17d2895d00ac19fe1dbb5695dff50
Anh Thang <buianhthang89@gmail.com>
2021-07-13 00:34:19 +0700

    fix: update Kap Haus instagram, discord

--- src/importer/kaphaus.js
+++ src/importer/kaphaus.js
@@ -3,8 +3,9 @@ const { launcher } = require('../utils');
 
 const scrap = scrapFrom('1QXedbahyT1FndM9QdxZ8HC0RshpbyCvChTuQTY688lE', {
   name: 'Kaphaus',
-  instagram: 'https://www.instagram.com/the.kaphaus/',
+  instagram: 'https://www.instagram.com/kap_haus/',
   website: 'https://geekhack.org/index.php?topic=85295.0',
+  discord: 'https://discord.gg/rmS2bgTFDU',
 });
 
 launcher(scrap);
