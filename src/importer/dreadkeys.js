99c26d7ca3d8e42a000d28c06bb3ecc0834ffc86
Anh Thang <buianhthang89@gmail.com>
2021-07-13 22:44:31 +0700

    Add Dreadkeys website (#74)
    * feat: add Dreadkeys website
    
    * fix: Dreadkeys discord invite
    
    * fix: update Kap Haus instagram, discord

--- src/importer/dreadkeys.js
+++ src/importer/dreadkeys.js
@@ -3,8 +3,9 @@ const { launcher } = require('../utils');
 
 const scrap = scrapFrom('1tzm7_NLV5XdM2tuM2naiZhDjMMygstN2XneBU_GWbeY', {
   name: 'Dreadkeys',
+  website: 'https://www.dreadkeys.com/',
   instagram: 'https://www.instagram.com/dreadkeys/',
-  discord: 'https://discord.gg/KDFs857bnA',
+  discord: 'https://discord.gg/cDdFgyYjWx',
 });
 
 launcher(scrap);
