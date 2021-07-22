612049c3db7e0305d307ed9691156ae2d943d5e5
mariotheguy <56885146+mariotheguy@users.noreply.github.com>
2021-07-22 11:50:33 -0400

    Added luna keycaps catalog (#78)

---
+++ src/importer/lunakeycaps.js
@@ -0,0 +1,15 @@
+const { scrapFrom } = require('../scraper/gdoc');
+const { launcher } = require('../utils');
+
+const scrap = scrapFrom('1AV5u4TtQY2iKOisKygGIn7IYFGzcqU9hhjqI76UzkbM', 
+  {
+    name: 'Luna Keycaps',
+    instagram: 'https://www.instagram.com/luna.keycaps/',
+  },
+);
+
+launcher(scrap);
+
+module.exports = {
+  scrap,
+};s
