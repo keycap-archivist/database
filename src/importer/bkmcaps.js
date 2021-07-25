22d9243f98b992bbd865c7b1650a9b69db865ea7
Anh Thang <buianhthang89@gmail.com>
2021-07-25 15:39:02 +0700

    fix: update BKM Caps discord (#83)

--- src/importer/bkmcaps.js
+++ src/importer/bkmcaps.js
@@ -5,6 +5,7 @@ const scrap = scrapFrom('1LinZIytdrqwGE7H3TBWeIrj6ICQ_2DzWVm2Mmk904Y8', {
   name: 'BKM Caps',
   instagram: 'https://www.instagram.com/bkmkeycaps/',
   website: 'https://bkmcaps.com/',
+  discord: 'https://discord.gg/sUEm9qtNAh',
 });
 
 launcher(scrap);
