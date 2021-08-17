268716ff14cc834b4980c1baa95a9cfa3c5fb1d0
Anh Thang <buianhthang89@gmail.com>
2021-08-17 18:58:27 +0700

    Add Alpha Keycaps new sculpts, beautify sculpt name (#91)
    * feat: add new sculpts, beauti sculpt name
    
    * fix: rename variable

--- src/importer/alphakeycaps.js
+++ src/importer/alphakeycaps.js
@@ -3,38 +3,41 @@ const axios = require('axios');
 const { launcher } = require('../utils');
 const { genId, sortBy } = require('../utils');
 
-const catalogsName = [
-  'keypora',
-  'jedi-blinker',
-  'blinker',
-  'matapora',
-  'alpha-ape',
-  'cherep',
-  'salvador',
-  'albison',
-  'mr-worldwide',
-];
+const catalogs = {
+  'mf-belooga': 'MF Belooga',
+  keypora: 'Keypora',
+  'jedi-blinker': 'Jedi Blinker',
+  blinker: 'Blinker',
+  matapora: 'Matapora',
+  'alpha-ape': 'Alpha Ape',
+  cherep: 'Cherep',
+  salvador: 'Salvador',
+  albison: 'Albison',
+  'mr-worldwide': 'Mr Worldwide',
+  'boosted-gamer-set': 'Boosted Gamer Set',
+};
+
 const BASE_URL = 'https://alphakeycaps.com/';
 
-async function CatalogParse(catName) {
-  const html = await axios.get(`${BASE_URL}${catName}`).then((res) => res.data);
+async function CatalogParse(catId) {
+  const html = await axios.get(`${BASE_URL}${catId}`).then((res) => res.data);
   const root = htmlparser.parse(html);
   const colorways = root.querySelectorAll('.intrinsic').map((e) => {
     const name = e.querySelector('.image-caption').querySelector('p').querySelector('strong').childNodes[0].rawText;
     const rawImg = e.querySelector('.thumb-image').rawAttrs;
     const m = /data-src="(.*?)"/.exec(rawImg);
     const imgsrc = m[1];
-    const re = new RegExp(catName.replace('-', ' '), 'gi');
+    const re = new RegExp(catId.replace('-', ' '), 'gi');
     return { name: name.replace(re, '').trim(), img: imgsrc, id: genId(imgsrc) };
   });
   return colorways;
 }
 
-async function GenSculpt(catname, sculptsArray) {
+async function GenSculpt([catId, catName], sculptsArray) {
   const s = {
-    name: catname.replace('-', ' '),
-    id: genId(`alpha-keycaps-${catname}`),
-    colorways: await CatalogParse(catname),
+    name: catName,
+    id: genId(`alpha-keycaps-${catId}`),
+    colorways: await CatalogParse(catId),
   };
   sculptsArray.push(s);
 }
@@ -51,8 +54,8 @@ async function scrap() {
       denySubmission: true,
       sculpts: [],
     };
-    for (const c of catalogsName) {
-      await GenSculpt(c, catalog.sculpts);
+    for (const cat of Object.entries(catalogs)) {
+      await GenSculpt(cat, catalog.sculpts);
     }
     catalog.sculpts = sortBy(catalog.sculpts, 'name');
     return catalog;
