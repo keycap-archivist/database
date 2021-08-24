d5cad966f09a62bdf1474a903b1e5b32f4655481
mariotheguy <mariodandrea14@yahoo.com>
2021-08-24 00:56:02 -0400

    Added klaykaps discord

--- src/importer/klaykaps.js
+++ src/importer/klaykaps.js
@@ -5,6 +5,7 @@ const scrap = scrapFrom('1U_Te4TY5Nxwme5CjnFMd3tGqSYXJxrkYK7c7VlkI2MU', {
   name: 'Klaykaps',
   instagram: 'https://www.instagram.com/klaykaps/',
   website: 'https://klaykaps.com/',
+  discord: 'https://www.discord.com/invite/TZDCwkr',
 });
 
 launcher(scrap);
