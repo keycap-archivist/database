e2e570853c26cf686f4ab99b4e0ecf95d6c45711
Vincent LE GOFF <vince.legoff@gmail.com>
2021-08-29 22:13:03 +0200

    ci: fix prune images (#100)
    * ci: fix prune images
    
    * fix
    
    * lint
    
    * ci: fix prune images
    
    * fix

--- src/prune-images.js
+++ src/prune-images.js
@@ -1,52 +1,77 @@
-const { execSync } = require('child_process');
+const { spawn, exec } = require('child_process');
 const db = require('../db/catalog.json');
 
-const imgs = [];
-const toDel = [];
-
 function getCurrentImages() {
-  return execSync(
-    'aws s3 ls s3://cdn.keycap-archivist.com/keycaps/ | grep ".jpg" | awk "{print $4}" | cut -d "." -f1',
-    { maxBuffer: 2000 * 1024 },
-  )
-    .toString()
-    .split('\n')
-    .map((x) => {
-      const str = x.trim();
-      if (!str) {
-        return '';
-      }
-      const arr = str.split(' ');
-      if (arr.length === 1) return str;
-      return arr[arr.length - 1].split('.')[0];
-    })
-    .filter(Boolean);
-}
-
-db.forEach((maker) => {
-  maker.sculpts.forEach((s) => {
-    s.colorways.forEach((c) => {
-      imgs[c.id] = { id: c.id, src: c.img };
+  return new Promise((res) => {
+    const cmd = spawn('aws', ['s3', 'ls', 's3://cdn.keycap-archivist.com/keycaps/']);
+    const cmdData = [];
+    cmd.stdout.on('data', (data) => {
+      cmdData.push(data.toString());
+    });
+    cmd.on('close', () => {
+      res(
+        cmdData
+          .filter(Boolean)
+          .join('')
+          .split('\n')
+          .map((x) => {
+            const re = /\b([\w.]+)$/gm;
+            const result = x.match(re);
+            if (result && result.length !== 0) {
+              return result[0].split('.')[0];
+            }
+            return '';
+          })
+          .filter(Boolean),
+      );
     });
   });
-});
+}
 
-const inDistant = getCurrentImages();
-for (const i of inDistant) {
-  if (!imgs[i]) {
-    toDel.push(i);
+async function promExec(cmd) {
+  return new Promise((res) => {
+    exec(cmd, (err, stdout) => {
+      console.log(stdout);
+      res(stdout);
+    });
+  });
+}
+
+async function main() {
+  const imgs = [];
+  const toDel = [];
+  db.forEach((maker) => {
+    maker.sculpts.forEach((s) => {
+      s.colorways.forEach((c) => {
+        imgs[c.id] = { id: c.id, src: c.img };
+      });
+    });
+  });
+
+  const inDistant = await getCurrentImages();
+  for (const i of inDistant) {
+    if (!imgs[i]) {
+      toDel.push(i);
+    }
+  }
+
+  console.log(`In db: ${Object.keys(imgs).length} images`);
+  console.log(`In distant: ${inDistant.length} images`);
+  console.log(`To Del: ${toDel.length} images`);
+
+  for (const i of toDel) {
+    const p = [];
+    p.push(promExec(`aws s3 rm s3://cdn.keycap-archivist.com/keycaps/${i}.jpg`));
+    p.push(promExec(`aws s3 rm s3://cdn.keycap-archivist.com/keycaps/720/${i}.jpg`));
+    p.push(promExec(`aws s3 rm s3://cdn.keycap-archivist.com/keycaps/250/${i}.jpg`));
+    await Promise.all(p);
   }
 }
 
-console.log(`In db: ${Object.keys(imgs).length} images`);
-console.log(`In distant: ${inDistant.length} images`);
-console.log(`To Del: ${toDel.length} images`);
-
-for (const i of toDel) {
-  const l1 = execSync(`aws s3 rm s3://cdn.keycap-archivist.com/keycaps/${i}.jpg`).toString();
-  console.log(l1);
-  const l2 = execSync(`aws s3 rm s3://cdn.keycap-archivist.com/keycaps/720/${i}.jpg`).toString();
-  console.log(l2);
-  const l3 = execSync(`aws s3 rm s3://cdn.keycap-archivist.com/keycaps/250/${i}.jpg`).toString();
-  console.log(l3);
-}
+main()
+  .then(() => {
+    console.log('Finished');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
